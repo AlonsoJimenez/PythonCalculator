9cb3a443cbb96e148d51e0a525a6aae54edd773f
Alonso <alonso.jimenez@hotmail.com>
2020-07-28 20:42:18 -0600

    Actualizacion host

--- helloworld/application.py
+++ helloworld/application.py
@@ -4,13 +4,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-
 def launch():
     return app.send_static_file('calculator.html')
 
 
 if __name__ == '__main__':
-    app.run(host="localhost",port="789", debug=True)
+    app.run(host="0.0.0.0",port="80", debug=True)
 
 '''
 def flaskrun(app, default_host="localhost", default_port="789"):
