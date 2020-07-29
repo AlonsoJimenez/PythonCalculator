2af11fbc72f7791ac6ed24305cfe59b6a0efc0e9
Alonso <alonso.jimenez@hotmail.com>
2020-07-28 20:15:45 -0600

    Prueba html

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,17 +1,40 @@
 #!flask/bin/python
-import json
-from flask import Flask, Response
-from helloworld.flaskrun import flaskrun
+from flask import Flask, render_template
+app = Flask(__name__)
 
-application = Flask(__name__)
+@app.rout('/')
+def launch():
+    return app.send_static_file('calculator.html')
 
-@application.route('/', methods=['GET'])
-def get():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
-
-@application.route('/', methods=['POST'])
-def post():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
 
 if __name__ == '__main__':
-    flaskrun(application)
+    flaskrun(app)
+
+def flaskrun(app, default_host="0.0.0.0", default_port="80"):
+    """
+    Takes a flask.Flask instance and runs it. Parses
+    command-line flags to configure the app.
+    """
+
+    # Set up the command-line options
+    parser = optparse.OptionParser()
+    msg = 'Hostname of Flask app [{}]'.format(default_host)
+    parser.add_option("-H", "--host",
+                      help=msg,
+                      default=default_host)
+    msg = 'Port for Flask app [{}]'.format(default_port)
+    parser.add_option("-P", "--port",
+                      help=msg,
+                      default=default_port)
+    parser.add_option("-d", "--debug",
+                      action="store_true", dest="debug",
+                      help=optparse.SUPPRESS_HELP)
+
+    options, _ = parser.parse_args()
+
+    app.run(
+        debug=True,
+        host=options.host,
+        port=int(options.port)
+    )
+
